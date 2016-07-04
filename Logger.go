c6f3e9be027426280c359f81105a5d097bfef0f8
Chidiebere Nnadi <chidiebere.nnadi@gmail.com>
2016-07-04 22:29:28 +0100

    Added logging and modified indentation of main file

--- Logger.go
+++ Logger.go
@@ -6,9 +6,16 @@ import (
   "time"
 )
 
-func Logger(inner http.Handler, name String) http.Handler {
-  return http.HandlerFunc(func(w http.ResponseWriter, *r http.Request) {
+func Logger(inner http.Handler, name string) http.Handler {
+  return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
     start := time.Now()
-    inner
+    inner.ServeHTTP(w, r)
+    log.Printf(
+      "%s\t%s\t%s\t%s",
+      r.Method,
+      r.RequestURI,
+      name,
+      time.Since(start),
+    )
   })
 }
